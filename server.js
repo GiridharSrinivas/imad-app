03c89d3bfe63684e3b9e2f043ce108cb7044b68b
Giridhar S <sgiridhar14@gmail.com>
2017-09-05 19:10:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/login.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 app.get('/ui/register.html', function (req, res) {
